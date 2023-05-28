69c799d7ea6ff0b73fbf0cdf8b42d9f90d954b44
Helena <v0robjeva.alena@yandex.ru>
2023-05-28 08:19:24 +0300

    chore: initial commit 16

--- src/test/java/ru/netology/web/CallbackTest.java
+++ src/test/java/ru/netology/web/CallbackTest.java
@@ -8,10 +8,12 @@ import org.openqa.selenium.By;
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import com.codeborne.selenide.Configuration;
 
 class CallbackTest {
     @Test
     void shouldTestVPositive() {
+        Configuration.headless = true;
         open("http://localhost:9999");
         $("span[data-test-id=name] input").setValue("Василий");
         $("span[data-test-id=phone] input").setValue("+79012345678");
